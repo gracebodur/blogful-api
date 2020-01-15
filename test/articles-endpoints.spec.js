c3e43f796cd0a441ca503a35c8fe464d50b53beb
Grace <mgracebodur@gmail.com>
2020-01-15 00:54:25 -0500

    /api prefix, PATCH request and update articles

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -21,11 +21,11 @@ describe('Articles Endpoints', function() {
 
     afterEach('cleanup', () => db('blogful_articles').truncate())
 
-    describe(`GET /articles`, () => {
+    describe(`GET /api/articles`, () => {
         context(`Given no articles`, () => {
             it('responds with 200 and an empty list', () => {
                 return supertest(app)
-                    .get('/articles')
+                    .get('/api/articles')
                     .expect(200, [])
             })
         })
@@ -41,18 +41,18 @@ describe('Articles Endpoints', function() {
 
             it('responds with 200 and all of the articles', () => {
                 return supertest(app)
-                .get('/articles')
+                .get('/api/articles')
                 .expect(200, testArticles)
             })
         })
      }) 
 
-    describe(`GET /articles/:article_id`, () => {
+    describe(`GET /api/articles/:article_id`, () => {
         context(`Given no articles`, () => {
             it(`responds with 404`, () => {
                 const articleId = 123456
                 return supertest(app)
-                    .get(`/articles/${articleId}`)
+                    .get(`/api/articles/${articleId}`)
                     .expect(404, { error: { message: `Article doesn't exist`}})
             })
         })
@@ -71,7 +71,7 @@ describe('Articles Endpoints', function() {
                 const articleId = 2
                 const expectedArticle = testArticles[articleId - 1]
                 return supertest(app)
-                .get(`/articles/${articleId}`)
+                .get(`/api/articles/${articleId}`)
                 .expect(200, expectedArticle)
                 .expect(res => {
                     const expected = new Date().toLocaleString('en', { timeZone: 'UTC'})
@@ -96,7 +96,7 @@ describe('Articles Endpoints', function() {
         
              it('removes XSS attack content', () => {
                return supertest(app)
-                 .get(`/articles/${maliciousArticle.id}`)
+                 .get(`/api/articles/${maliciousArticle.id}`)
                  .expect(200)
                  .expect(res => {
                    expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
@@ -107,7 +107,7 @@ describe('Articles Endpoints', function() {
         })
     })
 
-    describe(`POST /articles`, () => {
+    describe(`POST /api/articles`, () => {
            it(`creates an article, responding with 201 and the new article`,  function() {
             this.retries(3)
             const newArticle = {
@@ -116,7 +116,7 @@ describe('Articles Endpoints', function() {
                 content: 'Test new article content...'
             }
              return supertest(app)
-               .post('/articles')
+               .post('/api/articles')
                .send(newArticle)
                .expect(201)
                .expect(res => {
@@ -125,14 +125,14 @@ describe('Articles Endpoints', function() {
                     expect(res.body.content).to.eql(newArticle.content)
                     expect(res.body).to.have.property('id')
                     expect(res.body).to.have.property('date_published')
-                    expect(res.headers.location).to.eql(`/articles/${res.body.id}`)
+                    expect(res.headers.location).to.eql(`/api/articles/${res.body.id}`)
                     // const expected = new Date().toLocaleString('en', { timeZone: 'UTC'})
                     // const actual = new Date(res.body.date_published).toLocaleString()
                     // expect(actual).to.eql(expected)
                })
                .then(postRes =>
                 supertest(app)
-                    .get(`/articles/${postRes.body.id}`)
+                    .get(`/api/articles/${postRes.body.id}`)
                     .expect(postRes.body)
                 )
            })
@@ -148,7 +148,7 @@ describe('Articles Endpoints', function() {
                 it(`responds with 400 and an error message when the '${field}' is missing`, () => {
                 delete newArticle[field]
                 return supertest(app)
-                  .post('/articles')
+                  .post('/api/articles')
                   .send(newArticle)
                   .expect(400, {
                     error: { message: `Missing '${field}' in request body` }
@@ -156,12 +156,12 @@ describe('Articles Endpoints', function() {
               })
             })
 
-        describe(`DELETE /articles/:article_id`, () => {
+        describe(`DELETE /api/articles/:article_id`, () => {
             context(`Given no articles`, () => {
                  it(`responds with 404`, () => {
                    const articleId = 123456
                    return supertest(app)
-                     .delete(`/articles/${articleId}`)
+                     .delete(`/api/articles/${articleId}`)
                      .expect(404, { error: { message: `Article doesn't exist` } })
                  })
             })
@@ -179,13 +179,92 @@ describe('Articles Endpoints', function() {
                const idToRemove = 2
                const expectedArticles = testArticles.filter(article => article.id !== idToRemove)
                return supertest(app)
-                .delete(`/articles/${idToRemove}`)
+                .delete(`/api/articles/${idToRemove}`)
                 .expect(204)
                 .then(res =>
                    supertest(app)
-                    .get(`/articles`)
+                    .get(`/api/articles`)
                     .expect(expectedArticles)
                     )
+                })
+            })
+        })
+
+      describe.only(`PATCH /api/articles/:article_id`, () => {
+         context(`Given no articles`, () => {
+           it(`responds with 404`, () => {
+             const articleId = 123456
+             return supertest(app)
+               .patch(`/api/articles/${articleId}`)
+               .expect(404, { error: { message: `Article doesn't exist` } })
+           })
+         })
+
+         context('Given there are articles in the database', () => {
+           const testArticles = makeArticlesArray()
+      
+           beforeEach('insert articles', () => {
+             return db
+               .into('blogful_articles')
+               .insert(testArticles)
+           })
+      
+          it('responds with 204 and updates the article', () => {
+            const idToUpdate = 2
+            const updateArticle = {
+              title: 'updated article title',
+              style: 'Interview',
+              content: 'updated article content',
+            }
+             const expectedArticle = {
+              ...testArticles[idToUpdate - 1],
+              ...updateArticle
+            }
+             return supertest(app)
+              .patch(`/api/articles/${idToUpdate}`)
+              .send(updateArticle)
+              .expect(204)
+              .then(res =>
+              supertest(app)
+                .get(`/api/articles/${idToUpdate}`)
+                .expect(expectedArticle)
+             )
+           })
+
+          it(`responds with 400 when no required fields supplied`, () => {
+           const idToUpdate = 2
+           return supertest(app)
+             .patch(`/api/articles/${idToUpdate}`)
+             .send({ irrelevantField: 'foo' })
+             .expect(400, {
+               error: {
+                 message: `Request body must contain either 'title', 'style' or 'content'`
+               }
+             })
+          })
+
+          it(`responds with 204 when updating only a subset of fields`, () => {
+              const idToUpdate = 2
+              const updateArticle = {
+                title: 'updated article title',
+              }
+              const expectedArticle = {
+                ...testArticles[idToUpdate - 1],
+                ...updateArticle
+              }
+        
+              return supertest(app)
+                .patch(`/api/articles/${idToUpdate}`)
+                .send({
+                  ...updateArticle,
+                  fieldToIgnore: 'should not be in GET response'
+                })
+                .expect(204)
+                .then(res =>
+                  supertest(app)
+                    .get(`/api/articles/${idToUpdate}`)
+                    .expect(expectedArticle)
+                  )
                 })
             })
         })
